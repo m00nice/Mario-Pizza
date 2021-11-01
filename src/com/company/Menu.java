da81297639064ff48ab2ec4a266f177a0031ff56
m00nice <m00nice@live.com>
2021-11-01 14:04:37 +0100

    yo

---
+++ src/com/company/Menu.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Menu {
+
+    Pizza nummer1 = new Pizza("Vesuvio","tomatsauce, ost, skinke og oregano",57,1);
+    Pizza nummer2 = new Pizza("Amerikaner","tomatsauce, ost, oksefars og oregano",53,2);
+    Pizza nummer3 = new Pizza("Cacciatore","tomatsauce, ost, pepperoni og oregano",57,3);
+    Pizza nummer4 = new Pizza("Cabonara","tomatsauce, ost, kødsauce, spaghetti, cocktailpølser og oregano",63,4);
+    Pizza nummer5 = new Pizza("Dennis","tomatsauce, ost, skinke, pepperoni, cocktailpølser og oregano",65,5);
+    Pizza nummer6 = new Pizza("Bertil","tomatsauce, ost, Bacon og oregano",57,6);
+    Pizza nummer7 = new Pizza("Silvia","tomatsauce, ost, pepperoni, rød peber, løg, oliven og oregano",61,7);
+    Pizza nummer8 = new Pizza("Victoria","tomatsauce, ost, skinke, ananas, champignon og oregano",61,8);
+    Pizza nummer9 = new Pizza("Toronfo","tomatsauce, ost, skinke, Bacon, Kebab, chili og oregano",61,9);
+    Pizza nummer10 = new Pizza("Capricciosa","tomatsauce, ost, skinke, champignon og oregano",61,10);
+    Pizza nummer11 = new Pizza("Hawai","tomatsauce, ost, skinke, ananas og oregano",61,11);
+    Pizza nummer12 = new Pizza("Le Blissola","tomatsauce, ost, skinke, rejer og oregano",61,12);
+    Pizza nummer13 = new Pizza("Venezia","tomatsauce, ost, skinke, bacon og oregano",61,13);
+    Pizza nummer14 = new Pizza("Mafia","tomatsauce, ost, skinke, pepperoni, bacon, løg og oregano",61,14);
+    Pizza nummer15 = new Pizza("Salat Pizza","tomatsauce, ost, kebab, salat, creme fraiche og oregano",61,15);
+    Pizza nummer16 = new Pizza("name","toppings",61,16);
+    Pizza nummer17 = new Pizza("Charlie","tomatsauce, ost, gorgonzola,  spaghetti, kylling, rejer og oregano",63,17);
+    Pizza nummer18 = new Pizza("name","toppings",61,18);
+    Pizza nummer19 = new Pizza("name","toppings",61,19);
+    Pizza nummer20 = new Pizza("name","toppings",61,20);
+    Pizza nummer21 = new Pizza("Pratt","tomatsauce, ost, kylling, pepperoni, kødsauce, bacon og oregano",63,21);
+    Pizza nummer22 = new Pizza("name","toppings",61,22);
+    Pizza nummer23 = new Pizza("name","toppings",61,23);
+    Pizza nummer24 = new Pizza("name","toppings",61,24);
+    Pizza nummer25 = new Pizza("name","toppings",61,25);
+    Pizza nummer26 = new Pizza("name","toppings",61,26);
+    Pizza nummer27 = new Pizza("name","toppings",61,27);
+    Pizza nummer28 = new Pizza("name","toppings",61,28);
+    Pizza nummer29 = new Pizza("name","toppings",61,29);
+    Pizza nummer30 = new Pizza("name","toppings",61,30);
+
+}
---
+++ src/com/company/Pizza.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Pizza {
+
+    String name;
+    int price;
+    String toppings;
+    int number;
+
+    public Pizza(String name, String toppings, int price, int number) {
+        this.name = name;
+        this.price = price;
+        this.toppings = toppings;
+    }
+
+
+}
